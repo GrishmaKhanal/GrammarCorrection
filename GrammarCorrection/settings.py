85e77ca368e8d967abf57b13ddacc56cf4e87ff0
GrishmaKhanal <grishmakhanal48@gmail.com>
2024-01-19 11:55:19 +0545

    changed to deploy

--- GrammarCorrection/settings.py
+++ GrammarCorrection/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&4_pb6l2%kq6)t42z&%32dbmm8cp)ejr@v35e)x)&z6yq(^9^5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
